19863f0e114d4c64e559125b9efc76145e5c6989
PhamHuy <163984306+PhamHuy092004@users.noreply.github.com>
2024-05-18 17:00:33 +0700

    18/5/2024 hoan thanh model + giao dien trang chu, gioi thieu

--- DA_WebC5/DA_WebC5/wwwroot/js/site.js
+++ DA_WebC5/DA_WebC5/wwwroot/js/site.js
@@ -2,3 +2,34 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener('DOMContentLoaded', function() {
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function onScroll() {
+    var elementsLeft = document.querySelectorAll('.slide-in-left');
+    var elementsRight = document.querySelectorAll('.slide-in-right');
+
+    elementsLeft.forEach(function(el) {
+      if (isElementInViewport(el)) {
+        el.classList.add('active');
+      }
+    });
+
+    elementsRight.forEach(function(el) {
+      if (isElementInViewport(el)) {
+        el.classList.add('active');
+      }
+    });
+  }
+
+  window.addEventListener('scroll', onScroll);
+  onScroll(); // Kiểm tra ngay khi tải trang
+});
