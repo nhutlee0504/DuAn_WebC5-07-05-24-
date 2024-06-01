bfe6c871503e41a4885b2205d6b7140196742324
minhnhtpc06050 <nguyenminh21112004@gmail.com>
2024-06-01 14:23:19 +0700

    commit gd

--- DA_WebC5/DA_WebC5/obj/Debug/net5.0/DA_WebC5.AssemblyInfo.cs
+++ DA_WebC5/DA_WebC5/obj/Debug/net5.0/DA_WebC5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DA_WebC5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de5739cbd12a09730317235795d4da69cd586f72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc8df93630a34cecaac29ae5108c34aa0f2bab55")]
 [assembly: System.Reflection.AssemblyProductAttribute("DA_WebC5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DA_WebC5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
