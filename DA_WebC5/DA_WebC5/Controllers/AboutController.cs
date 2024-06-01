8840ba1648c119eda4342ade5bbb7bba21809758
nhutlee0504 <leminhnhut718@gmail.com>
2024-06-01 15:03:16 +0700

    Commit merge 1/6

---
+++ DA_WebC5/DA_WebC5/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DA_WebC5.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
