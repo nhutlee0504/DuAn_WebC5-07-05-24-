21cdc324806403e77cb7b9ceb449904a65c65259
minhdqpc06173 <minhdqpc06173@fpt.edu.vn>
2024-06-09 15:14:25 +0700

    commit 9/6

--- DA_WebC5/DA_WebC5/Controllers/CartController.cs
+++ DA_WebC5/DA_WebC5/Controllers/CartController.cs
@@ -7,43 +7,99 @@ using System.Linq;
 
 namespace DA_WebC5.Controllers
 {
-	public class CartController : Controller
-	{
-		private readonly ApplicationDbContext _context;
-		public CartController(ApplicationDbContext context)
-		{
-			_context = context;
-		}
-		public IActionResult Index()
-		{
-			return View(_context.Carts.Include(x => x.Account).Include(y => y.ProductDetails.Product));
-		}
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CartController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            string username = HttpContext.Session.GetString("LoggedInUser");
+            if (string.IsNullOrEmpty(username))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            ViewBag.Username = username;
+            return View(_context.Carts.Where(x => x.UserName == username)
+                .Include(x => x.Account).Include(y => y.ProductDetails.Product.Category));
+        }
 
-		public IActionResult AddCart(int IdPDetail)
-		{
-			string username = HttpContext.Session.GetString("LoggedInUser");
-			if (string.IsNullOrEmpty(username))
-			{
-				return RedirectToAction("Login", "Home");
-			}
-			else if (IdPDetail == 0)
-			{
+        public IActionResult AddCart(int IdPDetail, int quantity)
+        {
+            string username = HttpContext.Session.GetString("LoggedInUser");
+            if (string.IsNullOrEmpty(username))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            else if (IdPDetail == 0)
+            {
                 return NotFound();
             }
-			var prod = _context.ProductDetails.FirstOrDefault(x => x.IDPDetail == IdPDetail);
-			if (prod != null)
-			{
+            var prod = _context.ProductDetails.FirstOrDefault(x => x.IDPDetail == IdPDetail);
+            if (prod != null)
+            {
                 var cart = new Cart
                 {
                     IDPDetail = prod.IDPDetail,
                     UserName = username,
-                    Quantity = prod.Quantity
+                    Quantity = quantity
                 };
                 _context.Carts.Add(cart);
                 _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
-			return View();
+            return View();
         }
-	}
+
+        public IActionResult AddOne(int id)
+        {
+            var prod = _context.Carts.FirstOrDefault(x => x.IDCart == id);
+            if (prod != null) 
+            {
+                prod.Quantity++;
+                _context.SaveChanges(); 
+                return RedirectToAction(nameof(Index));
+            }
+            return NotFound();
+        }
+
+        public IActionResult SubtractOne(int id)
+        {
+            var prod = _context.Carts.FirstOrDefault(x => x.IDCart == id);
+            if (prod != null)
+            {
+                if (prod.Quantity > 1)
+                {
+                    prod.Quantity--;
+                    _context.SaveChanges();
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+              
+            }
+            return NotFound();
+        }
+
+        public IActionResult DeleteAll()
+        {
+            string username = HttpContext.Session.GetString("LoggedInUser");
+            var carts = _context.Carts.Where(x => x.UserName == username).ToList();
+            _context.RemoveRange(carts);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult DeleteProd(int id)
+        {
+            var prod = _context.Carts.Find(id);
+            _context.Remove(prod);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
 }
