d7e313d5acba6330896f87267e12db5569e07bc7
PhamHuy <163984306+PhamHuy092004@users.noreply.github.com>
2024-06-09 15:44:43 +0700

    Admin_Layout_2 _ 9/6/2024

--- DA_WebC5/DA_WebC5/obj/Debug/net5.0/DA_WebC5.RazorTargetAssemblyInfo.cs
+++ DA_WebC5/DA_WebC5/obj/Debug/net5.0/DA_WebC5.RazorTargetAssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyProductAttribute("DA_WebC5")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c722755f5de5dfb37d54a5a0e5dbb6dffff0ffb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+065de23cb6c6b260b434443ab7d441faff7aed5e")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DA_WebC5.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
