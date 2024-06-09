b24193d3e08ed8c9d0a229cb06957c1d8488737e
PhamHuy <163984306+PhamHuy092004@users.noreply.github.com>
2024-06-09 16:22:15 +0700

    Commit Admin _ 9 / 6

---
+++ DA_WebC5/DA_WebC5/Areas/Admin/Controllers/BillsController.cs
@@ -0,0 +1,22 @@
+﻿using DA_WebC5.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace DA_WebC5.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BillsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public BillsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [Route("Admin/Bills/Index")]
+        public IActionResult Index()
+        {
+            var list = _context.Bills.ToList();
+            return View(list);
+        }
+    }
+}
---
+++ DA_WebC5/DA_WebC5/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using DA_WebC5.Controllers;
+using DA_WebC5.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DA_WebC5.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class ProductsController : Controller
+	{
+
+		private readonly ApplicationDbContext _context;
+		public ProductsController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+		[Route("Admin/Products/Index")]
+		public IActionResult Index()
+		{
+			var list = _context.Products.ToList();
+			return View(list);
+		}
+        [Route("Admin/Products/Create")]
+        public IActionResult Create()
+		{
+            var productTypes = _context.Categories.ToList();
+            ViewBag.ProductTypes = new SelectList(productTypes);
+
+
+            return View();
+		}
+	}
+}
