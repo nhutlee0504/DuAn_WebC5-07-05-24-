5079de928b92688d932f59f3cc2f437e4ed67c2e
nhutlee0504 <leminhnhut718@gmail.com>
2024-06-16 11:34:51 +0700

    cm rieng t.minh

--- DA_WebC5/DA_WebC5/obj/Debug/net5.0/DA_WebC5.AssemblyInfo.cs
+++ DA_WebC5/DA_WebC5/obj/Debug/net5.0/DA_WebC5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DA_WebC5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9beb80e11cb1050f00cebf7eab6bc8cef2fbbb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be1ff1667341cd3c747b4f047f2f72f1df635597")]
 [assembly: System.Reflection.AssemblyProductAttribute("DA_WebC5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DA_WebC5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
