be1ff1667341cd3c747b4f047f2f72f1df635597
nhutlee0504 <leminhnhut718@gmail.com>
2024-06-16 10:49:53 +0700

    cm check 16/06

--- DA_WebC5/SportAPI/obj/Debug/net5.0/SportAPI.AssemblyInfo.cs
+++ DA_WebC5/SportAPI/obj/Debug/net5.0/SportAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+612ad023504bd135da13540e33897174c9f1c5df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9beb80e11cb1050f00cebf7eab6bc8cef2fbbb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
